02973326202b5b9a474bccc439bcff06084485d2
Gustavo do Carmo <gustavosoares.nh@gmail.com>
2023-05-23 15:29:11 -0300

    criacao de views e pagina inicial

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from catalogo.models import Livro, LivroInstancia, Autor, Genero
 
-# Create your views here.
+def index (request):
+    num_livros = Livro.objects.all().count()
+    num_livro_instancias = LivroInstancia.objects.all().count()
+
+    num_instancias_livre = LivroInstancia.objects.filter(status__exact='l').count()
+
+    num_autor = Autor.objects.count()
+
+    context = {
+        'num_livros': num_livros,
+        'num_livro_instancias': num_livro_instancias,
+        'num_instancia_livre': num_instancias_livre,
+        'num_autor': num_autor,
+    }
+
+    return render(request, 'index.html', context=context)
